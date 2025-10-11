ad263dcf0bf540c7d0f4c10bd7f3c69e76879fa0
rowangregory <rpgregory0404@jefcoed.com>
2025-10-11 12:57:40 -0500

    Create formtest.php

---
+++ formtest.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Submitted Results</title>
+  </head>
+  <body>
+    Your favorite adjective is <?php echo $_GET['status']; ?>
+  </body>
+</html>
