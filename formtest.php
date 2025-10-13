9ca2ba7d97e01d90fd2cfb486e01c3afa3b169be
rowangregory <rpgregory0404@jefcoed.com>
2025-10-13 10:40:23 -0500

    Update formtest.php

--- formtest.php
+++ formtest.php
@@ -1,11 +1,10 @@
-<?php
+<!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
     <title>Submitted Results</title>
   </head>
   <body>
-    <p>Your favorite adjective is</p> echo $_POST['status'];
+    <p>Your favorite adjective is</p> <?php echo $_POST['status']; ?>
   </body>
 </html>
-?>
